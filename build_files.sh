ad15cfcc30cb4a09790b579e2b399b57dd0b0dff
Kingunok <114995588+Kingunok@users.noreply.github.com>
2025-08-30 10:43:08 +0530

    Create build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing pip if required..."
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+echo "Building the project..."
+python3 -m pip install -r requirements.txt
+echo "Collect Static..."
+python3 manage.py collectstatic --noinput --clear
